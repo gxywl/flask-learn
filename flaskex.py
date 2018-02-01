29090a59f1630d197056b7dab516250d903cbe38
gxywl <gxywl@163.com>
2018-02-01 21:18:15 +0800

    p42

--- flaskex.py
+++ flaskex.py
@@ -1,35 +1,59 @@
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_script import Manager
+from flask_wtf import Form, FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required, DataRequired
 
 app = Flask(__name__)
 
-manager =Manager(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
-def hello_world():
-    #return '<h1>Hello World!</h1>'
-    return render_template('index.html', current_time=datetime.utcnow())
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # return '<h1>Hello World!</h1>'
+    # return render_template('index.html', current_time=datetime.utcnow())
+
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+
 
 @app.route('/user/<name>')
 def user(name):
-    #return '<h1>Hello, %s!</h1>' % name
+    # return '<h1>Hello, %s!</h1>' % name
     return render_template('user.html', name=name)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'),404
+    return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'),500
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    #要在Edit Config中加参数
+    # app.run(debug=True)
+    # 要在Edit Config中加参数
     manager.run()
