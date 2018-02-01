c6f2ce82fb1e40519d6d4d340738a38627317150
gxywl <gxywl@163.com>
2018-02-01 15:38:05 +0800

    p17

--- flaskex.py
+++ flaskex.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from flask_script import Manager
 
 app = Flask(__name__)
 
+manager =Manager(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return '<h1>Hello World!</h1>'
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
 
 if __name__ == '__main__':
-    app.run()
+    #app.run(debug=True)
+    #要在Edit Config中加
+    manager.run()
